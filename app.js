2af2ee83c40ed5cae9fd9a91e5f368c468b89120
Cody Williams <116775944+csw0374@users.noreply.github.com>
2023-05-15 23:31:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,65 +1,33 @@
-const words = [
-	'immunoelectrophoretically',
-	'rotavator',
-	'tsktsk',
-	'psychophysicotherapeutics',
-	'squirrelled',
-	'crypt',
-	'uncopyrightable',
-	'cysts',
-	'pseudopseudohypoparathyroidism',
-	'unimaginatively'
-];
+document.addEventListener("DOMContentLoaded", function() {
+let toDoForm= document.getElementById('todolist')
+let toDoList= document.getElementById('list')
 
-words.some(function(word) {
-	return word.length > 25;
+
+
+
+toDoForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "X";
+
+    let newToDo = document.createElement('li');
+    newToDo.innerText = document.getElementById("task").value;
+
+    toDoList.appendChild(newToDo);
+    newToDo.appendChild(removeButton);
+
+    toDoForm.reset();
+
 });
 
-words.some(function(word) {
-	return word.indexOf('thyroid') !== -1;
+toDoList.addEventListener('click', function(event){
+    const targetTagToLowerCase = event.target.tagName.toLowercase();
+    if (targetTagToLowerCase === 'li'){
+        event.target.style.textDecoration = "line-through";
+    } else if (targetTagToLowerCase === 'button') {
+        event.target.parentNode.remove();
+    }
+});
 });
 
-words.every(function(w) {
-	return w.length >= 5;
-});
-
-function allStrings(arr) {
-	return arr.every(function(el) {
-		return typeof el === 'string';
-	});
-}
-
-const btn = document.querySelector('button');
-btn.addEventListener('click', function(e) {
-	const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-	const allChecked = Array.from(checkboxes).every(function(checkbox) {
-		return checkbox.checked;
-	});
-	if (!allChecked) alert('PLEASE AGREE TO EVERYTHING!');
-});
-
-function mySome(arr, callback) {
-	for (let i = 0; i < arr.length; i++) {
-		if (callback(arr[i], i, arr)) return true;
-	}
-	return false;
-}
-
-mySome([ 4, 5, 6, 7 ], function(n) {
-	return n > 5;
-});
-
-function myEvery(arr, callback) {
-	for (let i = 0; i < arr.length; i++) {
-		if (!callback(arr[i], i, arr)) return false;
-	}
-	return true;
-}
-
-myEvery([ 4, 5, 6, 7 ], function(n) {
-	return n > 5;
-});
-
-myEvery([ 4, 5, 6, 7 ], function(n) {
-	return Number.isInteger(n);
-});
